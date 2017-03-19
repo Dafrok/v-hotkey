e7cce8cd61e83a5091c06dd9df89e64e179c47a1
Dafrok <o.o@mug.dog>
2017-03-19 10:34:18 +0800

    Get keymap handler

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 const getKeyCode = alias => {} 
 
 const getKeyMap = keymap => Object.keys(keymap).map(input => {
-  const result = {
-/*
-    key,
-    ctrl,
-    alt,
-    shift,
-    meta
-*/
-  }
+  const result = {}
+  input.split('+').forEach(keyName => {
+    const key = keyName === '.' ? [keyName] : keyName.split('.')
+    switch (key[0]) {
+      case 'ctrl':
+        result.ctrl = key[1] ? key[1] : true
+        break
+      case 'alt':
+        result.alt = key[1] ? key[1] : true
+        break
+      case 'shift':
+        result.shift = key[1] ? key[1] : true
+        break
+      case 'meta':
+        result.meta = key[1] ? key[1] : true
+        break
+      default:
+        result.key = getKeyCode(key[0])
+    }
+  })
+
+  return result
 })
 
 export default {
