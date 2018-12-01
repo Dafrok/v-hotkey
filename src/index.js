8c80ee530c479e2087b3657c43deb13d180652c7
Dafrok <o.o@mug.dog>
2018-12-02 01:38:46 +0800

    #9 dymanic keymaps

--- src/index.js
+++ src/index.js
@@ -22,29 +22,39 @@ const getKeyMap = keymap => Object.keys(keymap).map(input => {
   return result
 })
 
+function bindEvent (el, binding) {
+  el._keymap = getKeyMap(binding.value)
+  el._keyHandler = e => {
+    for (const hotkey of el._keymap) {
+      const callback = hotkey.keyCode === e.keyCode &&
+        !!hotkey.ctrl === e.ctrlKey &&
+        !!hotkey.alt === e.altKey &&
+        !!hotkey.shift === e.shiftKey &&
+        !!hotkey.meta === e.metaKey &&
+        hotkey.callback[e.type]
+      callback && callback(e)
+    }
+  }
+  document.addEventListener('keydown', el._keyHandler)
+  document.addEventListener('keyup', el._keyHandler)
+}
+
+function unbindEvent (el) {
+  document.removeEventListener('keydown', el._keyHandler)
+  document.removeEventListener('keyup', el._keyHandler)
+}
+
 export default {
   install (Vue) {
     Vue.directive('hotkey', {
-      bind (el, binding, vnode, oldVnode) {
-        el._keymap = getKeyMap(binding.value)
-        el._keyHandler = e => {
-          for (const hotkey of el._keymap) {
-            const callback = hotkey.keyCode === e.keyCode &&
-              !!hotkey.ctrl === e.ctrlKey &&
-              !!hotkey.alt === e.altKey &&
-              !!hotkey.shift === e.shiftKey &&
-              !!hotkey.meta === e.metaKey &&
-              hotkey.callback[e.type]
-            callback && callback(e)
-          }
+      bind: bindEvent,
+      componentUpdated (el, binding) {
+        if (binding.value !== binding.oldValue) {
+          unbindEvent.apply(this, arguments)
+          bindEvent.apply(this, arguments)
         }
-        document.addEventListener('keydown', el._keyHandler)
-        document.addEventListener('keyup', el._keyHandler)
       },
-      unbind (el, binding, vnode, oldVnode) {
-        document.removeEventListener('keydown', el._keyHandler)
-        document.removeEventListener('keyup', el._keyHandler)
-      }
+      unbind: unbindEvent
     })
   }
 }
