e2de2d77ed7edf2032bdc2a953c1d40fbdca560c
Dafrok <o.o@mug.dog>
2019-06-21 16:23:39 +0800

    fix: prepublish bug

--- build/webpack.config.prod.js
+++ build/webpack.config.prod.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './'),
-    filename: './index.js',
+    filename: '../index.js',
     library: 'VueHotkey',
     libraryTarget: 'umd'
   },
