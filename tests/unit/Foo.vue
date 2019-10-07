a59f316bef21d91d2332a87233549521a9b3aadd
Zdravko Ćurić <zcuric@gmail.com>
2019-10-07 14:53:44 +0200

    Implement basic tests

---
+++ tests/unit/Foo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div
+      v-hotkey="{ enter: show, esc: hide }"
+      class="hotkey"
+    >
+      Toggle
+    </div>
+    <div
+      v-if="visible"
+      class="visible"
+    >
+      Hello hotkey
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      visible: false
+    }
+  },
+  methods: {
+    show () {
+      this.visible = true
+    },
+    hide () {
+      this.visible = false
+    }
+  }
+}
+</script>
