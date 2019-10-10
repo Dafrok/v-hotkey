02d45f27954320f0b5152b1e07d01d32a1569bb6
Zdravko Ćurić <zcuric@gmail.com>
2019-10-10 16:35:10 +0200

    Refactoring and solve issues

--- src/keycodes/index.js
+++ src/keycodes/index.js
@@ -1,44 +1,93 @@
 import aliases from './aliases'
 import codes from './codes'
+import { splitCombination, returnCharCode } from '../helpers'
 
 const noop = () => {}
 
-const getKeyMap = (keymap, alias) => Object.keys(keymap).map(input => {
-  const result = {}
-  const { keyup, keydown } = keymap[input]
-  input.replace('numpad +', 'numpad add').split('+').forEach(keyName => {
-    switch (keyName.toLowerCase()) {
-      case 'ctrl':
-      case 'alt':
-      case 'shift':
-      case 'meta':
-        result[keyName] = true
-        break
-      default:
-        result.keyCode = alias[keyName] || searchKeyCode(keyName)
-    }
-  })
-  result.callback = {
-    keydown: keydown || (keyup ? noop : keymap[input]),
-    keyup: keyup || noop
-  }
-  return result
-})
+const defaultModifiers = {
+  ctrlKey: false,
+  altKey: false,
+  shiftKey: false,
+  metaKey: false
+}
 
+const alternativeKeyNames = {
+  option: 'alt',
+  command: 'meta',
+  return: 'enter',
+  escape: 'esc',
+  plus: '+',
+  mod: /Mac|iPod|iPhone|iPad/.test(navigator.platform) ? 'meta' : 'ctrl'
+}
+
+/**
+ *
+ * @param {Object} combinations
+ * @param {Object} alias
+ * @returns {Object}
+ */
+const getKeyMap = (combinations, alias) => {
+  const result = []
+
+  Object.keys(combinations).forEach(combination => {
+    const { keyup, keydown } = combinations[combination]
+    const callback = {
+      keydown: keydown || keyup ? noop : combinations[combination],
+      keyup: keyup || noop
+    }
+    const keys = splitCombination(combination)
+    const { code, modifiers } = resolveCodesAndModifiers(keys, alias)
+
+    result.push({
+      code,
+      modifiers,
+      callback
+    })
+  })
+
+  return result
+}
+
+/**
+ *
+ * @param {Array} keys
+ * @param {Object} alias
+ * @returns {Object}
+ */
+const resolveCodesAndModifiers = (keys, alias) => {
+  let modifiers = { ...defaultModifiers }
+  if (keys.length > 1) {
+    return keys.reduce((acc, key) => {
+      key = alternativeKeyNames[key] || key
+      if (defaultModifiers.hasOwnProperty(`${key}Key`)) {
+        acc.modifiers = { ...acc.modifiers, [`${key}Key`]: true }
+      } else {
+        acc.code = alias[key] || searchKeyCode(key)
+      }
+      return acc
+    }, { modifiers })
+  }
+
+  const key = alternativeKeyNames[keys[0]] || keys[0]
+  if (defaultModifiers.hasOwnProperty(`${key}Key`)) {
+    modifiers = { ...modifiers, [`${key}Key`]: true }
+  }
+  const code = alias[key] || searchKeyCode(key)
+
+  return {
+    modifiers,
+    code
+  }
+}
+/**
+ *
+ * @param {String} key
+ */
 const searchKeyCode = key => {
-  if (!key) return
-  // Keyboard Events
-  key = hasKeyCode(key) || String(key)
   return codes[key.toLowerCase()] ||
     aliases[key.toLowerCase()] ||
     returnCharCode(key)
 }
-
-const returnCharCode = key => key.length === 1 ? key.charCodeAt(0) : undefined
-const isPlainObject = obj => Object.prototype.toString.call(obj) === '[object Object]'
-const hasKeyCode = key => !isPlainObject(key)
-  ? key
-  : key.which || key.keyCode || key.charCode || false
 
 export {
   aliases,
